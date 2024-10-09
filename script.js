d4fc339da5763b601716223c81e36a2df3bfdbda
Narcio Silvestre <a21723@alunos.ipca.pt>
2024-10-09 04:35:03 +0100

    added carrossel

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const serv_items = document.querySelector('.nav_services');
 const sidebar = document.querySelector('.sidebar');
 const categories = document.querySelector('.categories');
 const show = document.querySelector('.show');
+const showImgs = show.querySelectorAll('img');
+const showFirImg = show.querySelector('img');
+const iconR = show.querySelector('.fa-angle-right');
+const iconL = show.querySelector('.fa-angle-left');
+const TRANSLATEX = 110;
+
+
+
 
 navservices.addEventListener('mouseover', () => {
     serv_items.classList.add('nav_servicesItem');
@@ -16,27 +24,102 @@ navservices.addEventListener('mouseleave', () => {
     serv_items.classList.remove('nav_servicesItem');
 })
 
-const arr = ['click', 'mouseenter',];
+const arr = ['mouseleave', 'mouseenter',];
 arr.forEach((el) => {
-    console.log(el)
     sidebar.addEventListener(el, (e) => {
-        if(e.target.classList.contains('fa-less-than')){
+        if(sidebar.classList.contains('sidebarMovein'))
             sidebar.classList.remove('sidebarMovein')
-        }
-        else{
+        else
             sidebar.classList.add('sidebarMovein');
-        }
     })
 });
 
-sidebar.addEventListener('mouseleave', () => {
-    sidebar.classList.remove('sidebarMovein');
+function setIconDisplay(side,state){
+    side === 'L' ? iconL.style.display = state : iconR.style.display = state 
+}
+
+function transformIMG(){
+    let counter = 0;
+    let trans = 0;
+    showImgs.forEach((img) => {
+        img.style.transform = `translatex(${trans}%)`
+        img.setAttribute('load','lazy')
+        img.setAttribute('data-show',counter === 0 ? 0 : trans)
+        img.setAttribute('data-number',counter)
+        img.style.opacity = `1`
+        trans += TRANSLATEX;
+        counter++;
+    })
+}
+
+function transformShow(side){
+    showImgs.forEach((img) => {
+        let curShow = parseInt(img.getAttribute('data-show'))
+        let nextShow = side === 'showL'? curShow - 110 : curShow + 110 
+        img.style.transform = `translatex(${nextShow}%)`
+        img.setAttribute('data-show',nextShow)
+    })
+}
+
+const arrIcons= [iconL,iconR]
+arrIcons.forEach((i)=>{
+    i.addEventListener('click', (e) => {
+        e.preventDefault()
+        transformShow(e.target.getAttribute('id')) 
+    })
 })
 
-show.addEventListener('mouseover', (e) => {
-    if(e.target.tagName === 'IMG')
-        e.target.classList.add('imgZoom');
+
+function observerLast(entries,observer){
+    let inter = false
+    entries.forEach((entry)=>{
+        let place = entry.target.dataset.show
+        let numImg = entry.target.dataset.number
+        let length = showImgs.length-1
+        if(entry.isIntersecting && (+place !== (length*TRANSLATEX))){
+            setIconDisplay('L','none')
+            setIconDisplay('R','block')
+            inter = true
+        } 
+        if(entry.isIntersecting && +place === 0 ){
+            setIconDisplay('R','none')
+            setIconDisplay('L','block')
+            inter = true
+        } 
+    })
+    if (inter === false) {
+        setIconDisplay('R','block')
+        setIconDisplay('L','block')
+    }
+}
+
+const imgObserver = new IntersectionObserver(observerLast,{
+    root:null,
+    threshold:0.1
 })
+
+window.addEventListener("load",()=>{
+    const arrImg= [showFirImg,showImgs[showImgs.length-1]]
+    arrImg.forEach((i)=>{
+        imgObserver.observe(i)
+    })
+})
+
+
+
+
+
+
+transformIMG()
+
+
+
+
+
+
+
+
+
 
 
 
